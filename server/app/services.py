a97f456b8a007926afe5ee237485314a5627bc15
Khasanjon Mamadaliyev <khasanjon.eng@gmail.com>
2023-04-26 16:36:56 +0500

    update files

--- server/app/services.py
+++ server/app/services.py
@@ -1,26 +1,12 @@
-import httpx
-
 from datetime import datetime
 
-from fastapi import UploadFile
-from fastapi.responses import UJSONResponse
 from sqlalchemy.orm import Session
 
 from app import schemas, models
 
 
 async def register_worker(schema: schemas.Register, db: Session):
-    data: dict = schema.dict(exclude_none=True)
-    if data.get('image'):
-        image = data.get('image')
-        image_url = uploading_image(image.read())
-        data.update({'image': image_url})
-    user = models.Masters(**data)
-    db.add(user)
-    db.commit()
-    db.close()
-    return UJSONResponse("Successful added user", status_code=200)
-
+    # save database
     pass
 
 
@@ -41,19 +27,3 @@ async def get_time_worker(db: Session, pk: int):
     # for _ in range(10):
     #     for user in master:
     #         if user.ordered_start ==
-
-
-def uploading_image(path_image):
-    result = httpx.post('https://telegra.ph/upload', files={'file': path_image}).json()
-    return result
-
-
-def save_image_worker(schema: schemas.Photo, db: Session):
-    data = schema.dict(exclude_none=True)
-    image = data.get('image')
-    image_url = uploading_image(image.read())
-    photo = models.Photos(image=image_url)  # noqa
-    db.add(photo)
-    db.commit()
-    db.close()
-    return UJSONResponse("Successfully added image", 200)
