9b352395b85ef66ae8eb94612a88f746b28e293f
MrShokhrukh <shokhrukhdot@gmail.com>
2023-05-05 19:47:51 +0500

    verify js

--- client/src/pages/auth/Verify.jsx
+++ client/src/pages/auth/Verify.jsx
@@ -1,17 +1,28 @@
+import axios from "axios";
 import React, { useRef } from "react";
 
 const Verify = () => {
-    let ref = useRef()
+  let ref = useRef();
+
+  const change = (e) => {};
 
   const submit = (e) => {
     e.preventDefault();
+    axios
+      .post("http://127.0.0.1:8000/activate-email", {})
+      .then((res) => {
+        ref = res.data;
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
     <div className="verify">
       <form onSubmit={submit}>
-        
-        <input type="number" placeholder="verification code" />
-
+        <input type="email" name="email" onChange={change} placeholder="email" />
+        <input type="number" name="code" onChange={change} placeholder="verification code" />
         <button>verify</button>
       </form>
     </div>
