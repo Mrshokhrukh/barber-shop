f67616b80a30b3500a4b2b34448b366d660557f9
MrShokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2023-05-11 03:21:16 +0500

    selected master

--- client/src/components/scheduled/ScheduleTime.jsx
+++ client/src/components/scheduled/ScheduleTime.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HiOutlineChevronRight, HiOutlineExclamationCircle } from "react-icons/hi";
 import CalendarDatePicker from "../calendar/Calendar";
 import "./masterAndDate.scss";
 
@@ -6,7 +7,29 @@ const ScheduleTime = () => {
   return (
     <div className="master-and-date">
       <div className="wrapper">
-        <div className="pickedMaster"></div>
+        <div className="pickedMaster">
+
+          <div className="master" >
+            <div className="left-in-responsive">
+              <div className="photo">
+                <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=1200&s=1" alt="404" />
+                <div>
+                  <HiOutlineExclamationCircle className="i" />
+                </div>
+              </div>
+              <div className="master_name">
+                <p className="name">shokhrukhabdvokhidov</p>
+                <p className="duty">Barber</p>
+              </div>
+            </div>
+
+            <div>
+              <HiOutlineChevronRight className="icon" />
+            </div>
+          </div>
+          
+        </div>
+
         <div className="date">
           <CalendarDatePicker />
         </div>
